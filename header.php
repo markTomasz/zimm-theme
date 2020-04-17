0d447f995590438576ff489e7ee76f234e79c3ea
Mark Thomas <mdvthomas@gmail.com>
2020-04-16 21:21:31 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package zimm
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'zimm' ); ?></a>
+
+	<?php if ( function_exists( 'the_custom_logo' ) ) : ?>
+		<?php the_custom_logo(); ?>
+	<?php endif; ?>
+
+	<div id="content" class="site-content">
